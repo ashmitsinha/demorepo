6c23603432522807247cfdede703fac3abd8ebc4
ashmit <ashmitsinha806@gmail.com>
2023-11-13 10:01:02 +0530

    first commit

---
+++ java1.java
@@ -0,0 +1,8 @@
+class java1{
+	public static void main(String args[]){
+		int a = 10;
+		int b = 69;
+		int c = a*b;
+		System.out.println(c);
+	}
+}
---
+++ py1.py
@@ -0,0 +1,5 @@
+print("hello bsdwalo")
+print("jenkins ki mkc")
+tsr = 127
+arr = 90
+print(tsr/arr)
